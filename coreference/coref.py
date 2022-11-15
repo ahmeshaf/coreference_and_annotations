9b77a9ce3562168a6d5ad9137f3ec89cf9e6669a
Abhijnan Nath <abhijnan@porsche.cs.colostate.edu>
2022-11-15 02:57:42 -0700

    coref experiment with triviality detector and cross encoder

--- coreference/coref.py
+++ coreference/coref.py
@@ -18,6 +18,19 @@ from coreference.incremental_clustering import incremental_clustering
 from sklearn.cluster import AgglomerativeClustering
 
 
+def load_data_pair_coref(trivial_non_trivial_path):
+    all_examples = []
+    with open(trivial_non_trivial_path) as tnf:
+        for line in tnf:
+            row = line.strip().split(',')
+            mention_pair = row[:2]
+            #triviality_label = int(row[3])
+            triviality_label = 0 if row[3] =='NEG' else 1
+            
+            all_examples.append((mention_pair, triviality_label))
+
+    return all_examples
+
 def get_cosine_similarities(mention_pairs, vector_map):
     """
     Calculate the cosine similarity as row wise dot product of 2 arrays of vectors
@@ -116,7 +129,7 @@ def get_mention_pair_similarity_lemma(mention_pairs, mention_map, relations, wor
     within_doc_similarities = []
 
     doc_sent_map = pickle.load(open(working_folder + '/doc_sent_map.pkl', 'rb'))
-    doc_sims = pickle.load(open(working_folder + '/doc_sims_path.pkl', 'rb'))
+    #doc_sims = pickle.load(open(working_folder + '/doc_sims_path.pkl', 'rb'))
     doc_ids = []
 
     for doc_id, _ in list(doc_sent_map.items()):
@@ -136,7 +149,7 @@ def get_mention_pair_similarity_lemma(mention_pairs, mention_map, relations, wor
             return len(set.intersection(arr1, arr2))/len(set.union(arr1, arr2))
 
         sent_sim = jc(set(men_map1['sentence_tokens']), set(men_map2['sentence_tokens']))
-        doc_sim = doc_sims[doc2id[men_map1['doc_id']], doc2id[men_map2['doc_id']]]
+        #doc_sim = doc_sims[doc2id[men_map1['doc_id']], doc2id[men_map2['doc_id']]]
         lemma_sim = float(men_map1['lemma'] in men_text2 or men_map2['lemma'] in men_text1)
 
         same_doc = float(men_map1['doc_id'] == men_map2['doc_id'])
@@ -235,6 +248,7 @@ def coreference(curr_mention_map, all_mention_map, working_folder,
 
     # sort event mentions and make men to ind map
     curr_mentions = sorted(list(curr_mention_map.keys()), key=lambda x: curr_mention_map[x]['m_id'])
+    print(len(curr_mentions))
     curr_men_to_ind = {eve: i for i, eve in enumerate(curr_mentions)}
 
     # generate gold clusters key file
@@ -256,10 +270,46 @@ def coreference(curr_mention_map, all_mention_map, working_folder,
             for j in range(i + 1):
                 if i != j:
                     mention_pairs.append((list_mentions[i], list_mentions[j]))
+    
+    
+    print(len(mention_pairs))  
+    # use generated pairs for triviality tests NOT NEEDED!
+    
+    # predict the labels for test pairs, separate the predicted trivial examples 
+    
+    
+    # send trivial to lemma and get simialities, but map them to their IDS 
+    
+    
+    #send the rest to cross encoder models and get scores i.e similarities,
+    
+    
+    #stack the arrays and then do cc clustering and get final results. 
+  
+    
+    
+#     parent_path = '/s/chopin/d/proj/ramfis-aida//coref/coreference_and_annotations/'
+#     triv_dev_path = parent_path + '/parsing/ecb/trivial_non_trivial_dev.csv'
+#     triv_test_path = parent_path + '/parsing/ecb/trivial_non_trivial_test.csv'
+#     with open("/s/chopin/d/proj/ramfis-aida/coref/coreference_and_annotations/coreference/test_indices", "rb") as fp:   
+#         test_indices = pickle.load(fp)
 
+#         test_pairs, test_labels = zip(*load_data_pair_coref(triv_test_path))
+
+#         test_pairs = [test_pairs[i] for i in test_indices]
+#         test_labels = [test_labels[i] for i in test_indices]
+    
+    
+    
     # get the similarities of the mention-pairs from either lemmas or cdlm embeddings
     if sim_type == 'lemma':
         similarities = get_mention_pair_similarity_lemma(mention_pairs, all_mention_map, relations, working_folder)
+         
+    
+    
+#     if sim_type == 'lemma':  #for triviality pairs
+#         similarities = get_mention_pair_similarity_lemma(test_pairs, all_mention_map, relations, working_folder)
+#         print(len(similarities))
     elif sim_type == 'cdlm':
         similarities = get_mention_pair_similarity_cdlm_bi(mention_pairs, all_mention_map, relations, working_folder)
     elif sim_type == 'cross-encoder': 
@@ -267,11 +317,17 @@ def coreference(curr_mention_map, all_mention_map, working_folder,
         similarities = [x for x in similarities.values()] #returns a dict with pairwise mention_ids as key, so only use scores 
     
     # get indices
+    #maybe change it for trivialilty pairs
     mention_ind_pairs = [(curr_men_to_ind[mp[0]], curr_men_to_ind[mp[1]]) for mp in mention_pairs]
+    #mention_ind_pairs = [(curr_men_to_ind[mp[0]], curr_men_to_ind[mp[1]]) for mp in test_pairs]
     rows, cols = zip(*mention_ind_pairs)
+    print(len(rows), len(cols))
 
     # create similarity matrix from the similarities
     n = len(curr_mentions)
+    
+    #change current mentions for triviality pairs 
+    #n = len(test_pairs)
     similarity_matrix = np.identity(n)
     similarity_matrix[rows, cols] = similarities
 
@@ -292,6 +348,8 @@ def coreference(curr_mention_map, all_mention_map, working_folder,
                                                                           top_n=top_n)
     else:
         raise AssertionError
+        
+        
     system_mention_cluster_map = [(men, clus) for men, clus in zip(curr_mentions, labels)]
 
     # generate system key file
